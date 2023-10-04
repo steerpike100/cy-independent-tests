421252b47c0eb209d4f0a156c8ef35f18944da46
steveb <steveb@motabilityoperations.co.uk>
2023-10-04 09:50:59 +0100

    SJB find consent button within iframe and click it

--- cypress/support/step_definitions/login.ts
+++ cypress/support/step_definitions/login.ts
@@ -4,6 +4,12 @@ Given('A published article link directs me to a the login page', () => {
 	cy.visit(
 		'https://www.independent.co.uk/extras/indybest/gadgets-tech/video-games-consoles/nintendo-switch-2-price-release-date-rumours-b2386412.html',
 	);
+	cy.get('iframe[title="SP Consent Message"]')
+		.should('be.visible')
+		.then(($iframe) => {
+			const $body = $iframe.contents().find('body');
+			cy.wrap($body).find('button[title="AGREE"]').click();
+		});
 });
 
 When('I log in succesfully', () => {});
